4b9e803b8dfda817f66d2fc845bd990ac5fedb95
Matheus <matheussantosrocha321@gmail.com>
2022-07-08 17:38:28 -0300

    Adicionando arquivos mar.py e run.py

---
+++ mar.py
@@ -0,0 +1,38 @@
+class Mar:
+    def __init__(self, classificao, minutos, quantidade_episodios=1):
+        self.__classificacao = classificao
+        self.__minutos = minutos
+        self.__quantidade_episodios = quantidade_episodios
+        self.minutos_totais = self.calcula_total_minutos()
+
+    @property
+    def classificacao(self):
+        return self.__classificacao
+
+    @property
+    def minutos(self):
+        return self.__minutos
+
+    @property
+    def quantidade_episodios(self):
+        return self.__quantidade_episodios
+
+
+    def calcula_total_minutos(self):
+        return self.__minutos * self.__quantidade_episodios
+
+    def __radd__(self, other):
+        return  other + self.minutos_totais
+
+class Suporte_mar:
+    def __init__(self):
+        self.__lista = []
+
+    def adicionar_programa(self, programa):
+        self.__lista.append(programa)
+
+    def imprimi_horas_de_mar(self):
+        return sum(self.__lista) // 60
+
+
+
---
+++ run.py
@@ -0,0 +1,38 @@
+from mar import Mar, Suporte_mar
+
+
+
+#cadastrando
+stranger_things = Mar('Serie', 45, 25)
+stranger_things_4 = Mar('Serie', 70, 6)
+outer_banks = Mar('Serie', 43, 12)
+the_umbrella_academy = Mar('Serie', 45, 23)
+riverdale = Mar('Serie', 41, 92)
+
+kanye_west_joe_rogan = Mar('Podcast', 100)
+miley_cyrus_joe_rogan = Mar('Podcast', 126)
+post_malone_joe_rogan = Mar('Podcast', 229)
+neil_deGrasse_joe_rogan = Mar('Podcast', 72)
+elon_musk_joe_rogan = Mar('Podcast', 81)
+wiz_khalifa_joe_rogan = Mar('Podcast', 123)
+
+
+
+#adicionando na lista
+suporte_mar = Suporte_mar()
+suporte_mar.adicionar_programa(stranger_things)
+suporte_mar.adicionar_programa(outer_banks)
+suporte_mar.adicionar_programa(stranger_things_4)
+suporte_mar.adicionar_programa(the_umbrella_academy)
+suporte_mar.adicionar_programa(riverdale)
+suporte_mar.adicionar_programa(wiz_khalifa_joe_rogan)
+
+suporte_mar.adicionar_programa(post_malone_joe_rogan)
+suporte_mar.adicionar_programa(miley_cyrus_joe_rogan)
+suporte_mar.adicionar_programa(neil_deGrasse_joe_rogan)
+suporte_mar.adicionar_programa(kanye_west_joe_rogan)
+suporte_mar.adicionar_programa(elon_musk_joe_rogan)
+
+#imprimindo horas de mar
+horas_de_mar = suporte_mar.imprimi_horas_de_mar()
+print(horas_de_mar)
